0f32a329a775e1b2ab6fad7c5bec227faa115c5e
hcnode <7372@163.com>
2017-07-11 15:48:10 +0800

    combine test cases

--- test/view.test.tsx
+++ test/view.test.tsx
@@ -1,37 +1,86 @@
-import * as React from 'react';
-import * as should from 'should';
+import * as should from 'should'
+import * as Koa from 'koa'
+import * as request from 'supertest-as-promised'
+import * as React from 'react'
 import { shallow, mount, render } from 'enzyme';
-import { initBrowser, chdir } from './util';
-var { createProvider } = require('../dist');
+import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+import inject from '../src/util/injectGlobal';
+import { chdir, initBrowser, loadScript } from './util';
+import * as webpack from "webpack";
+import * as fs from 'fs';
+import App from '../src/index'
+describe('#koa-cola view', function () {
+	var server, mongoose;
+	before(function (done) {
+		chdir();
+		initBrowser();
+		server = App();
+		mongoose = app.mongoose;
+		var Mockgoose = require('mockgoose').Mockgoose;
+		var mockgoose = new Mockgoose(mongoose);
+		// mockgoose.prepareStorage().then(function () {
+		app.mongoose.connect('mongodb://127.0.0.1:27017/koa-cola', function (err) {
+			done(err);
+		});
+		// });
+	});
 
+	after(function (done) {
+		delete global.app;
+		done();
+	})
+	describe('#', function () {
+		it("button", function () {
+			var click = false;
 
+			var CustomButton = require(`${process.cwd()}/views/components/button`).default;
+			var wrapper = mount(<CustomButton text="xxx" onClick={() => {
+				click = true;
+			}} />, { attachTo: document.getElementById('app') });
 
-describe("#tsx component", function () {
-    before(function (done) {
-        chdir();
-        initBrowser()
-        done();
-    });
+			wrapper.find('div button').node.innerHTML.should.be.equal('xxx')
+			wrapper.find('div button').length.should.be.equal(1);
+			wrapper.find('#isMounted').node.innerHTML.should.be.equal('true')
+			wrapper.find('#isMounted2').node.innerHTML.should.be.equal('cola!')
+			wrapper.find('div button').simulate('click')
+			should(click).be.equal(true);
+			wrapper.detach();
+		});
+		it('#build bundle', function (done) {
+			var config = require(`${process.cwd()}/webpack.config`);
+			webpack(config, (err, stats) => {
+				if (err || stats.hasErrors()) {
+					throw (new Error('webpack build error'))
+				}
+				done();
+			});
+		});
+		it('#load view and test client side react component', async function (done) {
 
-    after(function (done) {
-        delete global.app;
-        done();
-    })
-    it("button", function () {
-        var click = false;
-
-        var CustomButton = require(`${process.cwd()}/views/components/button`).default;
-        var wrapper = mount(<CustomButton text="xxx" onClick={() => {
-            click = true;
-        }} />, { attachTo: document.getElementById('app') });
-
-        wrapper.find('div button').node.innerHTML.should.be.equal('xxx')
-        wrapper.find('div button').length.should.be.equal(1);
-        wrapper.find('#isMounted').node.innerHTML.should.be.equal('true')
-        wrapper.find('#isMounted2').node.innerHTML.should.be.equal('cola!')
-        wrapper.find('div button').simulate('click')
-        should(click).be.equal(true);
-        wrapper.detach();
-    });
-
+			const { JSDOM } = require('jsdom');
+			const virtualConsole = new (require('jsdom').VirtualConsole)();
+			var dom = await JSDOM.fromURL(`http://127.0.0.1:${app.config.port}/cola`, {
+				virtualConsole: virtualConsole.sendTo(console),
+				runScripts: "dangerously",
+				features: {
+					FetchExternalResources: ["script"],
+					ProcessExternalResources: ["script"]
+				},
+				resources: "usable",
+			})
+			const { window } = dom;
+			const document = window.document;
+			var pepsi2 = require(`${process.cwd()}/views/pages/cola`).pepsi2;
+			window.onload = () => {
+				setTimeout(() => {
+					document.getElementById('dataFromServer').innerHTML.should.be.equal('hello')
+					document.getElementById('btn2').click();
+					should(document.getElementById('pepsi').innerHTML).be.equal(pepsi2);
+					document.getElementById('btn3').click();
+					should(document.getElementById('cola').innerHTML).be.equal('wow');
+					done();
+				}, 1000);
+			}
+		});
+	});
 });
