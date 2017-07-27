90fc9b54fb58597feb6c6885f2923ff4e9686d15
hcnode <7372@163.com>
2017-07-27 08:52:18 +0800

    test:npm version

--- dist/src/decorators/reducer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function Reducer(reducer) {
-    return function (target) {
-        target._reducer = reducer();
-    };
-}
-exports.Reducer = Reducer;
-;
