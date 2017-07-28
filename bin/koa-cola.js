f8a2d0cc7ad3b47ac3f7723ab2b13f65f2950699
hcnode <7372@163.com>
2017-07-28 12:02:01 +0800

    fix:add ts-node global depend to support cli

--- bin/koa-cola.js
+++ bin/koa-cola.js
@@ -1,15 +1,17 @@
-#!/usr/bin/env node
+#!/usr/bin/env ts-node
 
 var program = require('commander');
 var path = require('path');
 var fs = require('fs-extra');
 var shell = require('shelljs');
 var chalk = require('chalk');
+var createSchemaTypes = require('../dist/src/util/createSchemaTypes').default;
 
 program
 	.version('0.1.7')
 	.option('-n, --new [name]', 'new koa-cola project')
 	.option('-c, --cheer', 'build webpack bundle and launch app')
+	.option('-s, --schema', 'create model schemas')
 	.parse(process.argv);
 /**
  * 使用命令新建koa-cola项目，自动创建模版文件，并自动安装依赖，自动运行
@@ -35,6 +37,8 @@ if (program.new) {
 	}
 } else if(program.cheer){
 	shell.exec('webpack;koa-cola');
+} else if(program.schema){
+	createSchemaTypes();
 }else {
 	shell.exec(`${__dirname}/../node_modules/ts-node/dist/bin.js -F ${process.cwd()}/app.ts`);
 }
