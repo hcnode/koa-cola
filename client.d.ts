52978b2425787639ad6aaa284d2ca5f5296033aa
Harry Chen <hcnode@gmail.com>
2018-04-16 23:11:05 +0800

    Update client.d.ts

--- client.d.ts
+++ client.d.ts
@@ -1,7 +1,7 @@
-import { ChildrenComponents, header, bundle, doNotUseLayout, Cola, pageProps, autoRouter } from './src/decorators/views';
-import { createProvider } from './src/util/createRouter';
+import { ChildrenComponents, header, bundle, doNotUseLayout, Cola, pageProps, autoRouter } from './dist/src/decorators/views';
+import { createProvider } from './dist/src/util/createRouter';
 import * as store from 'redux-connect/lib/store';
-export { Base as ApiBase, fetch as apiFetch } from './src/util/api';
+export { Base as ApiBase, fetch as apiFetch } from './dist/src/util/api';
 export { createProvider, store, ChildrenComponents as include, header, bundle, doNotUseLayout, Cola, pageProps, autoRouter };
 export * from 'redux-connect';
 export * from 'controller-decorators';
