c79f6c02ead15a9720c51b87a9d58e5a9b48fa7d
陈志凌-zlchen <zlchen@game.ntes>
2019-03-01 14:24:27 +0800

    add node 10 to travis test

--- app_test/views/app.tsx
+++ app_test/views/app.tsx
@@ -1,48 +1,48 @@
 import * as React from 'react';
 import { render } from 'react-dom'
-import {createProvider} from '../../client'
+import { createProvider } from '../../client'
 
 var Provider = createProvider(
-    [
-	{
-		"component": "simple",
-		"path": "/simple"
-	},
-	{
-		"component": "cola",
-		"path": "/cola"
-	},
-	{
-		"component": "multiChildren",
-		"path": "/multiChildren"
-	},
-	{
-		"component": "autoLoadFromPages1",
-		"path": "/autoLoadFromPages1"
-	},
-	{
-		"component": "headerAndBundle",
-		"path": "/headerAndBundle"
-	},
-	{
-		"component": "pageProps",
-		"path": "/pageProps"
-	},
-	{
-		"component": "subPages/subPage",
-		"path": "/subPage"
+	[
+		{
+			"component": "simple",
+			"path": "/simple"
+		},
+		{
+			"component": "cola",
+			"path": "/cola"
+		},
+		{
+			"component": "multiChildren",
+			"path": "/multiChildren"
+		},
+		{
+			"component": "autoLoadFromPages1",
+			"path": "/autoLoadFromPages1"
+		},
+		{
+			"component": "headerAndBundle",
+			"path": "/headerAndBundle"
+		},
+		{
+			"component": "pageProps",
+			"path": "/pageProps"
+		},
+		{
+			"component": "subPages/subPage",
+			"path": "/subPage"
+		}
+	]
+	, {
+		'simple': require('./pages/simple').default,
+		'cola': require('./pages/cola').default,
+		'multiChildren': require('./pages/multiChildren').default,
+		'autoLoadFromPages1': require('./pages/autoLoadFromPages1').default,
+		'headerAndBundle': require('./pages/headerAndBundle').default,
+		'pageProps': require('./pages/pageProps').default,
+		'subPages/subPage': require('./pages/subPages/subPage').default,
 	}
-]
-,{
-    'simple' : require('./pages/simple').default,
-'cola' : require('./pages/cola').default,
-'multiChildren' : require('./pages/multiChildren').default,
-'autoLoadFromPages1' : require('./pages/autoLoadFromPages1').default,
-'headerAndBundle' : require('./pages/headerAndBundle').default,
-'pageProps' : require('./pages/pageProps').default,
-'subPages/subPage' : require('./pages/subPages/subPage').default,
-} 
-    , require('../config/reduxMiddlewares').reduxMiddlewares
+	, require('../config/reduxMiddlewares').reduxMiddlewares
 );
 
 render(<Provider />, document.getElementById('app'))
