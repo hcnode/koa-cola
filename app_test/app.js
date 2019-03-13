220692dd4763f4090fcf9c2bc04092c62dce7cf8
陈志凌-zlchen <zlchen@game.ntes>
2019-03-13 16:52:54 +0800

    update test demo

--- app_test/app.js
+++ app_test/app.js
@@ -1,4 +1,9 @@
+// require('@google-cloud/trace-agent').start({
+//   projectId: 'kinetic-bot-231610',
+//   keyFilename: '../../gdc-trace.json'
+// });
 require("ts-node/register");
+
 process.on("unhandledRejection", error => {
   console.log("unhandledRejection", require("util").inspect(error));
 });
