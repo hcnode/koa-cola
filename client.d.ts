d8e30952365a53e0c000399c780ac5ff0449fc53
陈志凌-zlchen <zlchen@game.ntes>
2019-03-13 16:51:17 +0800

    feat: validators

--- client.d.ts
+++ client.d.ts
@@ -1,7 +1,11 @@
 import { ChildrenComponents, header, bundle, doNotUseLayout, Cola, pageProps, autoRouter } from './dist/src/decorators/views';
 import { createProvider } from './dist/src/util/createRouter';
 import * as store from 'redux-connect-new/lib/store';
+import validateForm from './dist/src/util/validateForm'
+import validate from './dist/src/middlewares/validate'
 export { Base as ApiBase, fetch as apiFetch } from './dist/src/util/api';
 export { createProvider, store, ChildrenComponents as include, header, bundle, doNotUseLayout, Cola, pageProps, autoRouter };
 export * from 'redux-connect-new';
 export * from 'controller-decorators';
+export {validate} 
+export {validateForm}
